6d8213118350e33043f06ee0397fb6ff1f94492b
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2023-09-08 20:42:20 +0300

    implement express  + handlebars

--- server.js
+++ server.js
@@ -2,51 +2,49 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
-//const { engine } = require('express-handlebars');
 
 
-//app.engine('handlebars', engine());
-app.engine('handlebars', exphbs.engine());
+app.engine('handlebars', exphbs.engine({ defaultLayout:'main' }));
 app.set('view engine', 'handlebars');
 
 
+app.use(express.static('public'))
 
 // Routing 
 app.get('/', (req,res) => {
-    res.render("index", { layout: false });
+    res.render("index");
 });
 app.get('/about', (req,res) => {
-    res.render("about.", { layout: false });
+    res.render("about");
 });
 
 app.get('/contact', (req,res) => {
-    res.render("contact", { layout: false });
+    res.render("contact");
 });
 
 app.get('/login', (req,res) => {
-    res.render("login", { layout: false });
+    res.render("login");
 });
 
 app.get('/sign', (req,res) => {
-    res.render("sign", { layout: false });
+    res.render("sign");
 });
 
 
 
 
 app.get('/cars/ferrari', (req,res) => {
-    res.render("cars/ferrari", { layout: false });
+    res.render("cars/ferrari");
 });
 app.get('/cars/lamborghini', (req,res) => {
-    res.render("cars/lamborghini", { layout: false });
+    res.render("cars/lamborghini");
 });
 app.get('/cars/mercedes', (req,res) => {
-    res.render("cars/mercedes", { layout: false });
+    res.render("cars/mercedes");
 });
 app.get('/cars/porsche', (req,res) => {
-    res.render("cars/porsche", { layout: false });
+    res.render("cars/porsche");
 });
-
 
 
 
