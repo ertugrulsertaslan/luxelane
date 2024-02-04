029fcf70026e6a53d6adfe5f25782c114e1a4fcb
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-02-04 13:53:05 +0300

    add car detail page brand and add car and brand by id model

--- controllers/cars/carDetail.controller.js
+++ controllers/cars/carDetail.controller.js
@@ -2,7 +2,7 @@ import models from "../../models/index.js";
 
 export default async function(req,res)  {
     const carId = req.params.id;
-    const car = await models.cars.getById(carId);   
+    const car = await models.cars.getCarAndBrandById(carId);
     if (car) {
         res.render("cars/carDetail", car);
     } else {
