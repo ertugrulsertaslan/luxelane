f34b8bd119d406e939ca0238de2701566f9da3d5
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-02-04 13:56:04 +0300

    add get branch model and show on car add page

--- controllers/cars/getCaradd.controller.js
+++ controllers/cars/getCaradd.controller.js
@@ -1,6 +1,7 @@
 import models from "../../models/index.js";
 
 export default async function (req,res) {
+    const branchs = await models.branch.getBranch();
     const brands = await models.cars.getBrand();
-    res.render("cars/carAdd", { brands: brands });
+    res.render("cars/carAdd", { brands: brands , branchs : branchs });
 }
