8c7b6f11bbe277c7c2a3bb4b54cc5198ab2b5ca8
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-02-07 22:26:33 +0300

    add car type button in car list page

--- controllers/cars/carList.controller.js
+++ controllers/cars/carList.controller.js
@@ -3,5 +3,6 @@ import models from "../../models/index.js";
 export default async function (req,res) {
     let count = 20;
     const cars = await models.cars.getViewCars(count);
-    res.render("cars/carList", { cars: cars });
+    const cartypes = await models.cars.getCarType();
+    res.render("cars/carList", { cars: cars , cartypes : cartypes});
 }
