c6dec442693cf08842e9b7d501cf4d055a9962c3
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-02-13 22:26:59 +0300

    fix car create model

--- models/cars/createCar.model.js
+++ models/cars/createCar.model.js
@@ -1,14 +1,10 @@
 import { db } from "../../db.js";
 
 export default function(data,thumbnail) {
-
-
     return new Promise((resolve, reject) => { 
     const sql = `INSERT INTO car ('carTypeId', 'model','hp','seats','hourlyPrice','thumbnail','fuel','transmission','bodyType','doors','minDriverAge','minLicenseAge','zeroToHundredKmh', 'status', 'branchId', 'brandId') 
     VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
-    // !!! TODO: GET THIS VALUE FROM data PARAMETER
-    const branchId = 1;
-    db.run(sql,[data.carTypeId,data.model,data.hp,data.seats,data.hourlyPrice,thumbnail,data.fuel,data.transmission,data.bodyType,data.doors,data.minDriverAge,data.minLicenseAge,data.zeroToHundredKmh, data.status, branchId, data.brandId],(err, rows) => {
+    db.run(sql,[data.carTypeId,data.model,data.hp,data.seats,data.hourlyPrice,thumbnail,data.fuel,data.transmission,data.bodyType,data.doors,data.minDriverAge,data.minLicenseAge,data.zeroToHundredKmh, data.status, data.branchId, data.brandId],(err, rows) => {
         if (err) {
             reject(err);
         }
