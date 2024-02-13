315f8b2bca9e08893fdfcea1f713365f1cbe7f00
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-02-13 22:29:13 +0300

    fix sign up email control

--- controllers/customers/signUp.controller.js
+++ controllers/customers/signUp.controller.js
@@ -1,19 +1,26 @@
 import models from '../../models/index.js';
 
 export default async function(req, res) {
+
     const { email } = req.body;
     const user = await models.users.getByEmail(email);
-
     const data = req.body;
-   
-    if(user.email == data.email){
-        res.render('users/signUp', { errorMessage: 'Email has a already' });
-         
-    }else {
-        data.password = await models.users.passwordEncryption(data.password);
+
+   if(user){
+        if(user.email == data.email){
+            res.render('users/signUp', { errorMessage: 'Email has a already' });
+            
+        }else {
+            data.password = await models.users.passwordEncryption(data.password);
+            const tcPhoto = '/img/' + req.file.originalname;
+            await models.customers.create(data, tcPhoto); 
+            res.render('success', {message: 'User Added ', redirect: '/users/sign-up', layout: false, delay: 2000});   
+        }
+   }
+   else{
+    data.password = await models.users.passwordEncryption(data.password);
         const tcPhoto = '/img/' + req.file.originalname;
         await models.customers.create(data, tcPhoto); 
-        res.render('success', {message: 'User Added ', redirect: '/users/sign-up', layout: false, delay: 2000});   
-    }
-   
+        res.render('success', {message: 'User Added ', redirect: '/users/sign-up', layout: false, delay: 2000});
+   }
 }
