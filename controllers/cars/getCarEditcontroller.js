366e821f3b1ccdf45ca376ebf08736af090c4c54
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-02-21 23:45:05 +0300

    refactor car edit model and add car edit page brand,branchs,car types

--- controllers/cars/getCarEditcontroller.js
+++ controllers/cars/getCarEditcontroller.js
@@ -1,12 +1,16 @@
+
 import models from "../../models/index.js";
 
 export default async function(req,res)  { 
     
     const carId = req.params.id;
-    const car = await models.cars.getById(carId);
-
+    const car = await models.cars.getCarAndBrandById(carId);
+    const brand = await models.brand.getBrandById(car.brandId);
+    const branchs = await models.branch.getBranch();
+    const brands = await models.cars.getBrand();
+    const cartypes = await models.cars.getCarType();
     if (car) {
-        res.render("cars/carEdit",car);
+        res.render("cars/carEdit",{car,brand,branchs,brands,cartypes});
     } else {
         res.render("404");
     }
--- models/cars/updateCar.model.js
+++ models/cars/updateCar.model.js
@@ -2,10 +2,10 @@ import { db } from "../../db.js";
 
 export default function(id,data,thumbnail) {
     return new Promise((resolve, reject) => {
-      const sql = `UPDATE car  SET brand = ?,  model = ?,hp = ?,seats = ?,hourlyPrice = ?,thumbnail = ?,fuel = ?,
+      const sql = `UPDATE car  SET brandId = ?,  model = ?,hp = ?,seats = ?,hourlyPrice = ?,thumbnail = ?,fuel = ?,
       transmission = ?,bodyType = ?,doors = ?,minDriverAge = ?,minLicenseAge = ?,zeroToHundredKmh = ?
       WHERE id=?`;
-      db.run(sql,[data.brand,data.model,data.hp,data.seats,data.hourlyPrice,thumbnail,data.fuel,data.transmission,data.bodyType,data.doors,data.minDriverAge,data.minLicenseAge,data.zeroToHundredKmh,id],(err, rows) => {
+      db.run(sql,[data.brandId,data.model,data.hp,data.seats,data.hourlyPrice,thumbnail,data.fuel,data.transmission,data.bodyType,data.doors,data.minDriverAge,data.minLicenseAge,data.zeroToHundredKmh,id],(err, rows) => {
           if (err) {
               reject(err);
           }
