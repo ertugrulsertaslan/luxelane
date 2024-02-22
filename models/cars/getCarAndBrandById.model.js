2e2d989efc9456bab6c8f5aa31eb540974610d98
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-02-22 22:09:06 +0300

    Fix car and brand model

--- models/cars/getCarAndBrandById.model.js
+++ models/cars/getCarAndBrandById.model.js
@@ -2,7 +2,9 @@ import { db } from "../../db.js";
 
 export default function(id) {
     return new Promise((resolve, reject) => {
-        const sql = ` SELECT * FROM car INNER JOIN brand ON car.brandId = brand.id WHERE car.id=?;`;
+        const sql = ` SELECT car.id, car.model, car.hp, car.seats ,car.hourlyPrice, car.thumbnail, car.fuel, car.transmission,
+        car.bodyType, car.doors, car.minDriverAge, car.minLicenseAge, car.zeroToHundredKmh, car.status,
+        car.carTypeId, car.branchId, car.brandId, brand.name, brand.logo FROM car INNER JOIN brand ON brand.id = car.brandId WHERE car.id=?;`;
         db.all(sql,[id],(err, rows) => {
             if (err) {
                 reject(err);
