5ec861694a90642374f131b889a88104a9708a1d
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-11-21 23:34:13 +0300

    fix link problem

--- controllers/users/login.controller.js
+++ controllers/users/login.controller.js
@@ -1,27 +1,31 @@
 import models from "../../models/index.js";
 
 export default async function (req, res) {
+  const { email, password } = req.body;
+  const user = await models.users.getByEmail(email);
+  if (user) {
+    let passwordsEqual = await models.users.passwordCompare(
+      password,
+      user.password
+    );
 
-    const { email, password } = req.body;
-    const user = await models.users.getByEmail(email);
-    if (user) {
-        let passwordsEqual = await models.users.passwordCompare(password,user.password);
+    if (!passwordsEqual) {
+      res.render("users/login", {
+        errorMessage: "Email or password incorrect",
+      });
+    } else {
+      req.session.user = {
+        role: "CUSTOMER",
+        id: user.id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        password: user.password,
+      };
 
-        if(!passwordsEqual){
-            res.render('users/login', { errorMessage: 'Email or password incorrect' });
-        }else{
-        req.session.user = {
-            role: 'CUSTOMER',
-            id: user.id,
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            password:user.password,
-        };
-        
-        res.render("users/UserProfile", {user});
+      res.render("users/userProfile", { user });
     }
-    }else{
-        res.render('users/login', { errorMessage: 'Email or password incorrect' });
-    }
-} 
+  } else {
+    res.render("users/login", { errorMessage: "Email or password incorrect" });
+  }
+}
